c56ac47f624b9e96e9ce05e05bce2f15509abaeb
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-02-06 08:26:34 +0000

    Added a context menu [Work In Progress]

--- public/script.js
+++ public/script.js
@@ -1,5 +1,20 @@
-const noContext = document.getElementById('body');
+document.addEventListener("contextmenu", function (event) {
+	event.preventDefault();
+	var menu = document.getElementById("contextMenu");
+	menu.style.display = "block";
+	if (event.pageX + menu.offsetWidth > window.innerWidth) {
+		menu.style.left = (event.pageX - menu.offsetWidth) + "px";
+	} else {
+		menu.style.left = event.pageX + "px";
+	}
+	if (event.pageY + menu.offsetHeight > window.innerHeight) {
+		menu.style.top = (event.pageY - menu.offsetHeight) + "px";
+	} else {
+		menu.style.top = event.pageY + "px";
+	}
+});
 
-noContext.addEventListener('contextmenu', (e) => {
-  e.preventDefault();
+document.addEventListener("click", function (event) {
+	var menu = document.getElementById("contextMenu");
+	menu.style.display = "none";
 });
