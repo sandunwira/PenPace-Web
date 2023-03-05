a1697bc7a16d06975e533c34f97ede0e93db6712
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-03-05 09:32:06 +0000

    Latest commit

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname + "/public"), (_, res, next) => {
-	res.status(404)
-	res.sendFile(__dirname + "/404.html")
+	res.status(404);
+	res.redirect("/");
 });
 
 app.listen(8080);
--- public/script.js
+++ public/script.js
@@ -1,4 +1,5 @@
-document.addEventListener("contextmenu", function (event) {
+// CONTEXT MENU START ============================================================================ //
+document.addEventListener("contextmenu", function(event) {
 	event.preventDefault();
 	var menu = document.getElementById("contextMenu");
 	menu.style.display = "block";
@@ -14,7 +15,26 @@ document.addEventListener("contextmenu", function (event) {
 	}
 });
 
-document.addEventListener("click", function (event) {
+document.addEventListener("click", function(event) {
 	var menu = document.getElementById("contextMenu");
 	menu.style.display = "none";
 });
+// ============================================================================== CONTEXT MENU END //
+
+
+// SHORTCUT DISABLE START ======================================================================== //
+document.addEventListener("keydown", function(event) {
+	// Prevent default action for F5, Ctrl+R, and Ctrl+F5
+	if (event.keyCode === 116 || (event.ctrlKey && event.keyCode === 82)) {
+		event.preventDefault();
+	}
+	if (event.ctrlKey && event.keyCode === 116) {
+		event.preventDefault();
+		location.reload(true);
+	}
+	// Prevent default action for Ctrl+Shift+I
+	if (event.ctrlKey && event.shiftKey && event.keyCode === 73) {
+		event.preventDefault();
+	}
+});
+// ========================================================================== SHORTCUT DISABLE END //
