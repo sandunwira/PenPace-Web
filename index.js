7bd4967e0e96a0892e5ed3353ed90231820dbb87
Sandun Wiratunga <79461263+sandunwira@users.noreply.github.com>
2023-03-14 04:27:24 +0530

    Removed unnecessary packages

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.static(__dirname + "/public"), (_, res, next) => {
-	res.status(404);
-	res.redirect("/");
-});
-
-app.listen(8080);
